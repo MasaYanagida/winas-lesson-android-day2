3ea3b65b155a5ae7c284c5be3c8351f3ae3a1bca
k-fukuda <k-fukuda@winas.jp>
2021-03-05 08:34:27 +0900

    add: day2 homework

--- Day2Homework/app/src/main/java/com/winas_lesson/android/day2/homework/FrameLayoutActivity.kt
+++ Day2Homework/app/src/main/java/com/winas_lesson/android/day2/homework/FrameLayoutActivity.kt
@@ -1,8 +1,14 @@
 package com.winas_lesson.android.day2.homework
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.ActionProvider
 import androidx.viewbinding.ViewBinding
 import com.winas_lesson.android.day2.homework.databinding.FrameLayoutActivityBinding
+import kotlinx.android.synthetic.main.frame_layout_activity.view.*
 
 class FrameLayoutActivity: AbstractActivity(), ViewBindable {
     override lateinit var binding: ViewBinding
@@ -12,5 +18,33 @@ class FrameLayoutActivity: AbstractActivity(), ViewBindable {
         binding = FrameLayoutActivityBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val content = Content.create()
+
+        view.tvName.text = content.name
+        view.tvAddress.text = content.address
+        view.tvIntroduction.text = content.description
+        if (content.showFavoriteButton) {
+            view.btnFavorite.visibility = View.VISIBLE
+        } else {
+            view.btnFavorite.visibility = View.GONE
+        }
+
+        view.btnFavorite.setOnClickListener {
+            makeToast("お気に入りに登録しました")
+        }
+        view.btnImageChangeSize.setOnClickListener {
+            if (view.ivFig.layoutHeight == resources.getDimensionPixelSize(R.dimen.normal_height)) {
+                view.ivFig.layoutHeight = resources.getDimensionPixelSize(R.dimen.big_height)
+            } else {
+                view.ivFig.layoutHeight = resources.getDimensionPixelSize(R.dimen.normal_height)
+            }
+        }
+    }
+
+    private fun makeToast(text: String) {
+        val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.CENTER, 0, -200)
+        toast.show()
     }
 }
--- Day2Homework/app/src/main/java/com/winas_lesson/android/day2/homework/LinearLayoutActivity.kt
+++ Day2Homework/app/src/main/java/com/winas_lesson/android/day2/homework/LinearLayoutActivity.kt
@@ -1,11 +1,15 @@
 package com.winas_lesson.android.day2.homework
 
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Toast
 import androidx.viewbinding.ViewBinding
 import com.winas_lesson.android.day2.homework.databinding.LinearLayoutActivityBinding
 import com.winas_lesson.android.day2.homework.AbstractActivity
 import com.winas_lesson.android.day2.homework.Content
 import com.winas_lesson.android.day2.homework.ViewBindable
+import kotlinx.android.synthetic.main.frame_layout_activity.view.*
 
 class LinearLayoutActivity: AbstractActivity(), ViewBindable {
     override lateinit var binding: ViewBinding
@@ -17,6 +21,31 @@ class LinearLayoutActivity: AbstractActivity(), ViewBindable {
         setContentView(view)
 
         val content = Content.create()
-        // TODO : contentの内容をviewに反映させて、ボタンタップアクションを設定してください
+
+        view.tvName.text = content.name
+        view.tvAddress.text = content.address
+        view.tvIntroduction.text = content.description
+        if (content.showFavoriteButton) {
+            view.btnFavorite.visibility = View.VISIBLE
+        } else {
+            view.btnFavorite.visibility = View.GONE
+        }
+
+        view.btnFavorite.setOnClickListener {
+            makeToast("お気に入りに登録しました")
+        }
+        view.btnImageChangeSize.setOnClickListener {
+            if (view.ivFig.layoutHeight == resources.getDimensionPixelSize(R.dimen.normal_height)) {
+                view.ivFig.layoutHeight = resources.getDimensionPixelSize(R.dimen.big_height)
+            } else {
+                view.ivFig.layoutHeight = resources.getDimensionPixelSize(R.dimen.normal_height)
+            }
+        }
+    }
+
+    private fun makeToast(text: String) {
+        val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.CENTER, 0, -200)
+        toast.show()
     }
 }
