a2ce96bd414db53786bea55fbae14ba21753129f
yuichi sugiura <sugiura@winas.jp>
2021-03-14 20:09:41 +0900

    課題実装

--- Day2Homework/app/src/main/java/com/winas_lesson/android/day2/homework/MainActivity.kt
+++ Day2Homework/app/src/main/java/com/winas_lesson/android/day2/homework/MainActivity.kt
@@ -7,9 +7,12 @@ import android.view.animation.LinearInterpolator
 import android.view.animation.Transformation
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.viewbinding.ViewBinding
 import com.winas_lesson.android.day2.homework.databinding.ActivityMainBinding
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
 import timber.log.Timber
 import kotlin.properties.Delegates
 
@@ -17,9 +20,17 @@ class MainActivity : AbstractActivity(), ViewBindable {
     override lateinit var binding: ViewBinding
     // readonly property
     private val toggleButton: Button?
-        get() = (binding as? ActivityMainBinding)?.button
+        get() = (binding as? ActivityMainBinding)?.toggleButton
+    private val favoriteButton: Button?
+        get() = (binding as? ActivityMainBinding)?.buttonFavorite
     private val imageView: ImageView?
         get() = (binding as? ActivityMainBinding)?.imageView
+    private val textName: TextView?
+        get() = (binding as? ActivityMainBinding)?.textName
+    private val textAddress : TextView?
+        get() = (binding as? ActivityMainBinding)?.textAddress
+    private val textDescription : TextView?
+        get() = (binding as? ActivityMainBinding)?.textDescription
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +38,22 @@ class MainActivity : AbstractActivity(), ViewBindable {
         val view = binding.root
         setContentView(view)
         //setContentView(R.layout.activity_main)
+        val content = Content.create()
+        textName?.text = content.name
+        textAddress?.text = content.address
+        textDescription?.text = content.description
+        this.favoriteButton!!.visibility = if (content.showFavoriteButton) View.VISIBLE else View.GONE
 
-        button?.setOnClickListener {
+
+
+        toggleButton?.setOnClickListener {
             toggle()
         }
+
+        favoriteButton?.setOnClickListener(){
+            Toast.makeText(applicationContext,"お気に入りに登録しました",Toast.LENGTH_LONG).show()
+        }
+
     }
 
     private var isExpand: Boolean = false
